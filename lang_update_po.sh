6a8cc5c61f17159d505528cd284b0fd92995ce97
Eduard Braun <Eduard.Braun2@gmx.de>
2015-10-09 02:53:53 +0200

    Update lang_update_po.sh (delete old files)

--- lang_update_po.sh
+++ lang_update_po.sh
@@ -3,9 +3,11 @@
 cd 0.91/
 mv -f de.po de_old.po
 msgmerge de_old.po inkscape.pot > de.po
+rm de_old.po
 cd ..
 
 cd trunk/
 mv -f de.po de_old.po
 msgmerge de_old.po inkscape.pot > de.po
+rm de_old.po
 cd ..
