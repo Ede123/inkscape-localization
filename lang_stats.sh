6a76debcd81d9827d5fe27462edf640a454bf216
Eduard Braun <eduard.braun2@gmx.de>
2017-08-04 21:04:47 +0200

    Rename trunk -> master

--- lang_stats.sh
+++ lang_stats.sh
@@ -2,5 +2,5 @@
 
 msgfmt --statistics -cv 0.91/de.po
 msgfmt --statistics -cv 0.92/de.po
-msgfmt --statistics -cv trunk/de.po
+msgfmt --statistics -cv master/de.po
 rm -f messages.mo
--- lang_update_mo.sh
+++ lang_update_mo.sh
@@ -2,4 +2,4 @@
 
 msgfmt 0.91/de.po -o 0.91/inkscape.mo
 msgfmt 0.92/de.po -o 0.92/inkscape.mo
-msgfmt trunk/de.po -o trunk/inkscape.mo
+msgfmt master/de.po -o master/inkscape.mo
