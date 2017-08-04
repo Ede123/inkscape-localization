ca735d2dfbe4bff0b7f2c23a145c2b1d7a26c391
Eduard Braun <eduard.braun2@gmx.de>
2017-08-04 23:45:48 +0200

    Rename trunk -> master (forgot one)

--- lang_update_po.sh
+++ lang_update_po.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if msgmerge --version > /dev/null; then
-	for folder in 0.91 0.92 trunk; do
+	for folder in 0.91 0.92 master; do
 		cd $folder
 		if msgmerge de.po inkscape.pot > de_new.po; then
 			mv -f de_new.po de.po
