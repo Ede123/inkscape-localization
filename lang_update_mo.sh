3ab36463a641d174a11733cc4c063479c4670532
Yaron Shahrabani <sh.yaron@gmail.com>
2018-07-25 11:01:38 +0300

    Add Hebrew alongside German

--- lang_update_mo.sh
+++ lang_update_mo.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-msgfmt 0.91/de.po -o 0.91/inkscape.mo
-msgfmt 0.92/de.po -o 0.92/inkscape.mo
-msgfmt master/de.po -o master/inkscape.mo
+lang_arr=(de he)
+for lang in ${lang_arr[@]}
+do
+msgfmt 0.91/$lang.po -o 0.91/inkscape.mo
+msgfmt 0.92/$lang.po -o 0.92/inkscape.mo
+msgfmt master/$lang.po -o master/inkscape.mo
+done
