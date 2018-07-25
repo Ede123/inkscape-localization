4cef7595d5ef2d72db74dc41143819e85007bd81
Yaron Shahrabani <sh.yaron@gmail.com>
2018-07-25 11:04:31 +0300

    Added Hebrew alongside German

--- lang_stats.sh
+++ lang_stats.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-msgfmt --statistics -cv 0.91/de.po
-msgfmt --statistics -cv 0.92/de.po
-msgfmt --statistics -cv master/de.po
+lang_add=(de he)
+
+for lang in ${lang_arr[@]}
+do
+msgfmt --statistics -cv 0.91/$lang.po
+msgfmt --statistics -cv 0.92/$lang.po
+msgfmt --statistics -cv master/$lang.po
 rm -f messages.mo
+done
